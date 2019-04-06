327dd8f3329f8608cb9c50cf2bf6c43d9dd67cae
Veronika Svítková <veron.svitkova@gmail.com>
2019-04-07 01:07:53 +0200

    Pridane errory v implementacii

--- mat_knihovna.py
+++ mat_knihovna.py
@@ -12,18 +12,30 @@ def Multiply(a, b):
     return a*b
 
 def Divide(a, b):
+    if b==0:
+        raise ZeroDivisionError("Nie je mozne delit nulou!")
     return a/b
 
 def Factorial(a):
+    if a<0 or isinstance(a, float):
+        raise ValueError("Faktorial je definovany len pre kladne cele cisla!")
     return math.factorial(a)
 
 def Power(x, y):
+    if a<0 or isinstance(a, float):
+        raise ValueError("Exponent musi byt kladne cele cislo!")
     return x ** y
 
 def Odmocnina(x, y):
+    if x<0 and (y%2 ==0):
+        raise ValueError("Zaklad nemoze byt zaporny pre parne odmocniny!")
+    if y<=0 or isinstance(y, float):
+        raise ValueError("Odmocnina musi byt kladne cele cislo!")
     return x ** (1/y)
 
 def Log(x):
+    if x <= 0:
+        raise ValueError("Zaklad logaritmu musi byt kladne cislo!")
     return math.log10(x)
 
 
