a1776fcbdb632c39c7b7d24f82f7b6698b81c02a
xdvora2z <xdvora2z@stud.fit.vutbr.cz>
2019-04-06 14:08:10 +0200

    Create test_mat_knihovna.py
    Testy matematické knihovny

---
+++ test_mat_knihovna.py
@@ -0,0 +1,104 @@
+#Spusteni pres prikazovou radku:
+#pytest [Param] test_mat_knihovna.py 
+
+import mat_knihovna.py
+import pytest
+
+def test_Plus():
+    assert mat_knihovna.Plus(0,0)==0
+    assert mat_knihovna.Plus(10,-10)==0
+    assert mat_knihovna.Plus(5,8)==13
+    assert mat_knihovna.Plus(-5,-8)==-13
+    assert mat_knihovna.Plus(250,-120)==130
+    assert mat_knihovna.Plus(120,-250)==-130
+    assert mat_knihovna.Plus(5000000,5000000)==10000000
+    assert mat_knihovna.Plus(5000000,-1000000)==4000000
+    assert mat_knihovna.Plus(0.25,0.25)==0.5
+    assert mat_knihovna.Plus(0.123456,0)==0.123456
+
+def test_Minus():
+    assert mat_knihovna.Minus(0,0)==0
+    assert mat_knihovna.Minus(10,10)==0
+    assert mat_knihovna.Minus(5,8)==-3
+    assert mat_knihovna.Minus(15,8)==7
+    assert mat_knihovna.Minus(-5,-8)==3
+    assert mat_knihovna.Minus(250,-120)==370
+    assert mat_knihovna.Minus(-120,-250)==130
+    assert mat_knihovna.Minus(5000000,-5000000)==10000000
+    assert mat_knihovna.Minus(5000000,1000000)==4000000
+    assert mat_knihovna.Minus(0.75,0.25)==0.5
+    assert mat_knihovna.Minus(0.123456,0)==0.123456
+    assert mat_knihovna.Minus(0,0.123456)==-0.123456
+
+def test_Multiply():
+    assert mat_knihovna.Multiply(5,5)==25
+    assert mat_knihovna.Multiply(-5,5)==-25
+    assert mat_knihovna.Multiply(-5,-5)==25
+    assert mat_knihovna.Multiply(5,0)==0
+    assert mat_knihovna.Multiply(0,5)==0
+    assert mat_knihovna.Multiply(0,0)==0
+    assert mat_knihovna.Multiply(5,5)==25
+    assert mat_knihovna.Multiply(2018,2019)==4074342
+    assert mat_knihovna.Divide(526894,58965)==31068304710
+    assert mat_knihovna.Multiply(5,0.2)==1
+    assert mat_knihovna.Divide(0.3425,0.12345)==0.042281625
+    assert mat_knihovna.Divide(0.3425,-0.12345)==-0.042281625
+
+def test_Divide():
+    assert mat_knihovna.Divide(25,5)==5
+    assert mat_knihovna.Divide(25,-5)==-5
+    assert mat_knihovna.Divide(-25,-5)==5
+    assert mat_knihovna.Divide(5,5)==1
+    assert mat_knihovna.Divide(-5,-5)==1
+    assert mat_knihovna.Divide(15,8)==1.875
+    assert mat_knihovna.Divide(0,5)==0
+    assert mat_knihovna.Divide(25,-50)==-0.5
+    assert mat_knihovna.Divide(4000000,-2000000)==-2
+    assert mat_knihovna.Divide(0.324,0.48)==0.675
+    assert mat_knihovna.Divide(0.324,-0.48)==-0.675
+    with pytest.raises(ZeroDivisionError):
+        mat_knihovna.Divide(5,0)
+        mat_knihovna.Divide(1234.5678,0)
+        mat_knihovna.Divide(-500,0)
+
+def test_Factorial():
+    assert mat_knihovna.Factorial(0)==1
+    assert mat_knihovna.Factorial(1)==1
+    assert mat_knihovna.Factorial(8)==40320
+    assert mat_knihovna.Factorial(18)==6402373705728000
+    with pytest.raises(ValueError):
+        mat_knihovna.Factorial(-5)
+        mat_knihovna.Factorial(-5.85)
+        mat_knihovna.Factorial(5.123456789)
+
+def test_Power():
+    assert mat_knihovna.Power(0,0)==1
+    assert mat_knihovna.Power(5,0)==1
+    assert mat_knihovna.Power(-5,0)==1
+    assert mat_knihovna.Power(5,1)==5
+    assert mat_knihovna.Power(5,-1)==0.2
+    assert mat_knihovna.Power(5,5)==3125
+    assert mat_knihovna.Power(5,-5)==0.00032
+    assert mat_knihovna.Power(5,10)==9765625
+    assert mat_knihovna.Power(18,18)==3.934640807529654e+22
+    assert mat_knihovna.Power(18,-18)==2.5415280553343456e-23
+    with pytest.raises(ValueError):
+        mat_knihovna.Power(0,-2)
+        mat_knihovna.Power(-10,-3.5)
+        mat_knihovna.Power(-10,3.5)
+
+def test_Odmocnina():
+    assert mat_knihovna.Odmocnina(0,2)==0
+    assert mat_knihovna.Odmocnina(1,20)==1
+    assert mat_knihovna.Odmocnina(1,-20)==1
+    assert mat_knihovna.Odmocnina(4,2)==2
+    assert mat_knihovna.Odmocnina(4,-2)==0.5
+    assert mat_knihovna.Odmocnina(8,3)==2
+    assert mat_knihovna.Odmocnina(8,-3)==0.5
+    assert mat_knihovna.Odmocnina(0.58,2)==0.7615773105863908
+    assert mat_knihovna.Odmocnina(0.58,-2)==1.3130643285972257
+    assert mat_knihovna.Odmocnina(1000000,8)==5.623413251903491
+    assert mat_knihovna.Odmocnina(1000000,-8)==0.1778279410038923
+    with pytest.raises(ValueError):
+        mat_knihovna(-5,2)
+        mat_knihovna(-5,-2)
