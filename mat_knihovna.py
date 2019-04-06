305f9f4f1de51689f0d251d1ba5b39cf5ee80233
Veronika Svítková <veron.svitkova@gmail.com>
2019-04-06 23:31:35 +0200

    Pridana prva verzia mat. kniznice

--- mat_knihovna.py
+++ mat_knihovna.py
@@ -1,3 +1,30 @@
 #implementacia mat. kniznice
 
+import math
+
+def Plus(a, b):
+    return a+b
+
+def Minus(a, b):
+    return a-b
+
+def Multiply(a, b):
+    return a*b
+
+def Divide(a, b):
+    return a/b
+
+def Factorial(a):
+    return math.factorial(a)
+
+def Power(x, y):
+    return x ** y
+
+def Odmocnina(x, y):
+    return x ** (1/y)
+
+def Log(x):
+    return math.log10(x)
+
+
 
